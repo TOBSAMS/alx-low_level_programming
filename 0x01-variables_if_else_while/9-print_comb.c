c6c199f373f7c3caeb9b8eaf5af8a884e85138e6
Omoniyi Oluwatobi <omoniyioluwatobi222@gmail.com>
2022-09-09 21:12:11 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,18 @@
-#include <stdio.h>
+nclude <stdio.h>
 /**
  * main - entry point
  * Return: alway returns 0
  */
 int main(void)
 {
-unsigned char a = '0';		
 int i;
 
-for (i = 0; i < 10; i++)
+for (i = 10; i < 20; i++)
 {
-putchar(a);
-a++;
+putchar((i % 10) + '0');
+if (i != 19)
+{														         putchar(',');											                          putchar(' ');
 }
-a = '1';
-for (i = 0; i < 6; i++)
-{
-putchar('0' + a);
-a++;
 }
 putchar('\n');
 return (0);
