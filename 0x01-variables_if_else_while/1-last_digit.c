ece2380a31ebf9e891431f3287f5e58e53af6087
Omoniyi Oluwatobi <omoniyioluwatobi222@gmail.com>
2022-09-09 19:36:04 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if(n > 5)
 {
-	printf("%d and is greater than 5\n", n);
+	printf("%s and is greater than 5\n", n);
 }
 else if(n == 0)
 {
-	printf("%d and is 0\n", n);
+	printf("%s and is 0\n", n);
 }
 else if(n < 6)
 {
-	printf("%d and is less than 6 and not 0\n", n);
+	printf("%s and is less than 6 and not 0\n", n);
 }
 return (0);
 }
