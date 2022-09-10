b329b4c1e70ccb03560b25d3dce1d07a1802a83f
Omoniyi Oluwatobi <omoniyioluwatobi222@gmail.com>
2022-09-09 21:44:32 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+nclude <stdio.h>
 
 /**
- * main - Prints all possible combinations of two two-digit numbers
+ * main - Prints all possible combinations of two two-digit numbers,
  * Return: Always 0.
  */
 int main(void)
@@ -10,7 +10,7 @@ int num1, num2;
 for (num1 = 0; num1 <= 98; num1++)
 {
 for (num2 = num1 + 1; num2 <= 99; num2++)
-{		{
+{
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
 putchar(' ');
