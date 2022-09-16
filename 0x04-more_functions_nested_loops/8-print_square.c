90687453490c50422f2c58d09f5ef07bc97f62f9
Omoniyi Oluwatobi <omoniyioluwatobi222@gmail.com>
2022-09-16 01:12:36 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,6 +4,7 @@
  * print_square - prints a square, followed by a new line;
  * @size: size of the square
  */
+
 void print_square(int size)
 {
 	if (size <= 0)
@@ -12,7 +13,9 @@ void print_square(int size)
 	}
        	else
 	{
+
 		int i, j;
+
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
