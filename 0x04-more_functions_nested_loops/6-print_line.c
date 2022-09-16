3c95f0c588d408bf2aa28d32fcb65749b70a40c7
Omoniyi Oluwatobi <omoniyioluwatobi222@gmail.com>
2022-09-16 00:58:31 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
  */
+
 void print_line(int n)
 {
 	if (n <= 0)
