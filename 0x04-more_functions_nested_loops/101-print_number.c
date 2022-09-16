6baeb6d01ef3b80f9561125060898456136a1330
Omoniyi Oluwatobi <omoniyioluwatobi222@gmail.com>
2022-09-16 01:21:12 -0700

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,6 +10,7 @@ void print_number(int n)
 {
 	unsigned int x;
 	x = n;
+
 	if (n < 0)
 	{
 		_putchar(45);
@@ -17,5 +18,5 @@ void print_number(int n)
 	}
 	if (x / 10)
 		print_number(x / 10);
-	_putchar((x % 10) + '0');
+ 		_putchar((x % 10) + '0');
 }
