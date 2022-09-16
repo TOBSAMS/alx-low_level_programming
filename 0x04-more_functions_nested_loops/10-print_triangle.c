3cd2d8812de6726cfac4faff668280b44618ce75
Omoniyi Oluwatobi <omoniyioluwatobi222@gmail.com>
2022-09-16 01:14:50 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
+
 void print_triangle(int size)
 {
 	if (size <= 0)
@@ -11,6 +13,7 @@ void print_triangle(int size)
 	} else
 	{
 		int i, j;
+
 		for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
