82bb2ff36ccaf5d487813ca017941a87a5a1979e
Omoniyi Oluwatobi <omoniyioluwatobi222@gmail.com>
2022-09-22 14:03:28 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,40 +1,25 @@
 #include "main.h"
 
 /**
+ * _strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ * @dest: String that will be appended
+ * @src: String to be concatenated upon
  *
- * _strcat - concatenate two strings
- *
- * @dest: string to be appended to
- *
- * @src: string to append
- *
- * Return: concatenated string
- *
+ * Return: returns poiner to @dest
  */
 
 char *_strcat(char *dest, char *src)
 
 {
+		int index = 0, dest_len = 0;
 
-	int i = 0;
+			while (dest[index++])
+				dest_len++;
 
-	int j = 0;
+			for (index = 0; src[index]; index++)
+				dest[dest_len++] = src[index];
 
-
-	while (dest[i] != '\0')
-
-		i++;
-		while (src[j] != '\0')
-
-		{
-
-			dest[i] = src[j];
-			i++;
-			j++;
-
-		}
-		dest[i] = '\0';
-
-		return (dest);
+			return (dest);
 
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,35 +1,20 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
-
-
-char *_strcat(char *dest, char *scr);
-
-char *_strncat(char *dest, char *scr, int n);
-
-char *_strncpy(char *dest, char *scr, int n);
-
-int _strncmp(char *s1, char *s2);
+#include <stdio.h>
 
 void reverse_array(int *a, int n);
-
-char *string_toupper(char *);
-
-char *cap_string(char *);
-
-char *leet(char *);
-
-char *rot13(char *);
-
 void print_number(int n);
-
-int _putchar(char c);
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
 void print_buffer(char *b, int size);
-
-
+int _strcmp(char *s1, char *s2);
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif
