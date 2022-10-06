a9ac0d3bba1d78a035c9bf51e7c3931533765a9b
Omoniyi Oluwatobi <omoniyioluwatobi222@gmail.com>
2022-10-05 21:46:34 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,53 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * str_concat - concats strings
- * @s1: string 1
- * @s2: string 2
- * Return: returns concated string
+ * str_concat - A function that concatenates two strings
+ * @s1: An input pointer of the first string
+ * @s2: An input pointer of the second string
+ * Return: Apointer to concatened strings or NULL if it str is NULL
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, len1, len2
-	char *conc;
+	char *new_str, *starts1, *starts2;
+	int i = 0, lens1 = 0, lens2 = 0;
 
-	i = 0;
-	len1 = 0;
-	len2 = 0;
+	starts1 = s1;
+	starts2 = s2;
 	if (s1 == NULL)
 		s1 = "";
+	while (*s1)
+	{
+		lens1++;
+		s1++;
+	}
+	s1 = starts1;
 	if (s2 == NULL)
 		s2 = "";
-
-	while (s1[i] != '\0')
+	while (*s2)
 	{
-		i++;
-		len1++;
+		lens2++;
+		s2++;
 	}
-
-	i = 0;
-
-	while (s2[i] != '\0')
-	{
-		i++;
-		len2++;
-	}
-
-	conc = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (conc == NULL)
+	s2 = starts2;
+	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	starts1 = new_str;
+	if (new_str == NULL)
 		return (NULL);
-
-	for (i = 0; i < len1; i++)
+	for (; i < (lens1 + lens2); i++)
 	{
-		conc[i] = s1[i];
+		if (i < lens1)
+		{
+			new_str[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[i] = *s2;
+			s2++;
+		}
 	}
-
-	for (i = 0; i < len2; i++)
-	{
-		conc[i + len1] = s2[i];
-	}
-	conc[i + len1] = '\0';
-	return (conc);
+	new_str[i] = '\0';
+	return (starts1);
 }
